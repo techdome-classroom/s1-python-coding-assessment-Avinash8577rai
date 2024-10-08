88f963af77e6c7cc573f5e3b53a0a7b8b6a98234
Ankit_kumar <cls>
2024-10-08 13:05:12 +0530

    Done

--- program1.py
+++ program1.py
@@ -2,5 +2,36 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
-                    
-        return 0
+        if not grid or not grid[0]:
+            return 0
+        
+        rows, cols = len(grid),len(grid[0])
+        
+        visited=[[False for _ in range(cols)] for _ in range(rows)]
+        
+        #Helper function
+
+        def DFS(r,c):
+            #Base case
+            if r<0 or c<0 or r>=rows or c>= cols:
+                return
+            if grid[r][c]=='W' or visited[r][c]:
+                return
+            
+            visited[r][c]=True
+            
+            #Checking all four direction
+            
+            DFS(r+1,c)
+            DFS(r-1,c)
+            DFS(r,c+1)
+            DFS(r,c-1)
+        
+        count = 0
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] =='L' and not visited[i][j]:
+                    DFS(i,j)
+                    count+=1             
+        return count
--- program2.py
+++ program2.py
@@ -1,5 +1,21 @@
 def decode_message( s: str, p: str) -> bool:
 
 # write your code here
-  
-        return False
+        m,n = len(s), len(p)
+        
+        table = [[False]*(n+1) for _ in range(m+1)]
+        
+        #Base Case
+        table[0][0]=True
+        
+        for j in range(1,n+1):
+                if p[j-1]=='*':
+                        table[0][j]=table[0][j-1]
+                        
+        for i in range(1,m+1):
+                for j in range(1,n+1):
+                        if p[j-1]==s[i-1] or p[j-1]=='?':
+                                table[i][j]=table[i-1][j-1]
+                        elif p[j-1]=='*':
+                                table[i][j]=table[i][j-1] or table[i-1][j]
+        return table[m][n]
